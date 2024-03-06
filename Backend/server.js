2cfeabb659dfdbe3bb8f0cf0fbc10d5a6b96f9c6
n273b <nishthabarnwal273@gmail.com>
2024-03-06 19:09:13 +0530

    Added custom strings implementation

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const shortid = require('shortid')
 const ShortUrl = require('./models/Url')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -21,6 +22,9 @@ app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // routes
 app.get('/', (req, res) => {
     res.render('index')
@@ -29,24 +33,52 @@ app.get('/', (req, res) => {
 app.post('/', async (req, res) => {
     try {
         // url is defined within object as string input of object 'req.body' is then assigned to variable url
-        const { url } = req.body
-        const click_me = 'Click on the short link to the open the website'
+        const { url, text } = req.body
+        const textString = JSON.stringify(text);
         if(!url){
             throw new Error('Request body is empty')
         }
+
+        // If custom shortid is provided
+        if (text) {
+            // Check if custom shortid already exists
+            const customShortIdExists = await ShortUrl.findOne({ shortid: textString });
+            if (customShortIdExists) {
+                throw new Error('Custom shortid already exists');
+            }
+            const shortUrl = new ShortUrl({ url: url, shortid: textString }); // Use custom shortid
+            const result = await shortUrl.save();
+            res.render('index', { short_url: `http://localhost:5000/${result.shortid}`, click_me: 'click this link' });
+        } else {
+            // If no custom shortid provided, generate one
+            const shortUrl = new ShortUrl({ url: url, shortid: shortid.generate() });
+            const result = await shortUrl.save();
+            res.render('index', { short_url: `http://localhost:5000/${result.shortid}`, click_me: 'click this link' });
+        }
+
         const urlExists = await ShortUrl.findOne({ url })
         if (urlExists) {
-            res.render('index', { short_url: `http://localhost:5000/${urlExists.shortid}`, click_me: 'click me' })
+            res.render('index', { short_url: `http://localhost:5000/${urlExists.shortid}`, click_me: 'click this link' })
             return
         }
-        const shortUrl = new ShortUrl({ url: url, shortid: shortid.generate() })
-        const result = await shortUrl.save()
-        res.render('index', { short_url: `http://localhost:5000/${result.shortid}`, click_me: 'click me' })
+        // const shortUrl = new ShortUrl({ url: url, shortid: shortid.generate() })
+        // const result = await shortUrl.save()
+        // res.render('index', { short_url: `http://localhost:5000/${result.shortid}`, click_me: 'click this link' })
     } catch (error) {
-        console.log('An error occurred:', error.message);        
+        console.log('An error occurred:', error.message);
+        res.status(400).send(error.message); // Send error response to client
     }
-
 })
+
+// app.post('/', async(req, res) => {
+//     const {text} = req.body
+//     if(!text){
+//         console.log("Custom string filed is empty")
+//     }else{
+//         const result = await ShortUrl({ url: url, shortid: text })
+//         res.render('index', { short_url: `http://localhost:5000/${result.shortid}`, click_me: 'click this link' })
+//     }
+// })
 
 app.get('/:shortid', async(req, res) => {
     try {
